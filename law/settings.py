1ac329d122b0b5447bf77992dddf0a6ce9352467
Joseph James <77192718+josephjames123@users.noreply.github.com>
2024-02-29 14:02:12 +0530

    port settings

--- law/settings.py
+++ law/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*%jad^o4gud+9b)_&fkm&n)z4jd&$^758boq7nd6tw7yy+xlna
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
